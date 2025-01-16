7dc40708ebd5b249ad3f81cf319fd721e6b4eba1
martinaegidius <gygridd@gmail.com>
2025-01-16 10:58:47 +0100

    added dataset fields in config.yaml for hydra, and added else-dataset agnostic dataset testing

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,37 @@
-from torch.utils.data import Dataset
 
-from cleaninbox.data import MyDataset
+import os 
 
+import omegaconf
+import pytest 
+import torch 
 
-def test_my_dataset():
+from cleaninbox.data import text_dataset
+from tests import _PROJECT_ROOT, _PATH_DATA
+
+#@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
+def test_text_dataset():
     """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
+    cfg = omegaconf.OmegaConf.load(f"{_PROJECT_ROOT}/configs/config.yaml")
+    N_TRAIN_SAMPLES = cfg.dataset.num_train_samples
+    N_TEST_SAMPLES = cfg.dataset.num_test_samples 
+    N_CLASSES = cfg.dataset.num_labels
+    
+    train, test = text_dataset()
+    assert isinstance(train,torch.utils.data.TensorDataset)
+    assert isinstance(test,torch.utils.data.TensorDataset)
+
+    assert len(train)==N_TRAIN_SAMPLES, "obtained train dataset length not corresponding to length in configuration"
+    assert len(test)==N_TEST_SAMPLES, "obtained train dataset length not corresponding to length in configuration"
+    
+    #test dataset sequence lengths
+    for dataset in [train,test]:
+        for input_ids, token_type_ids, attention_mask, labels in dataset:
+            assert input_ids.shape==token_type_ids.shape==attention_mask.shape, "mismatch in sequence length"
+            assert labels.dim() == 0, "Labels should be scalars"
+            assert labels in range(N_CLASSES), "Detected labels outside of configuration number of classes."
+
+    train_targets = torch.unique(train.tensors[-1])
+    assert (train_targets==torch.arange(0,N_CLASSES)).all(), "Not all train targets are represented in train targets"
+    test_targets = torch.unique(test.tensors[-1])
+    assert (test_targets==torch.arange(0,N_CLASSES)).all(), "Not all test targets are represented in test targets"
+    
