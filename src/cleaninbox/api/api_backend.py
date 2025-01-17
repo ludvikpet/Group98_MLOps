287862a223138940481b0d8eeddb11fbff00357d
ludvikpet <ludvikpet@gmail.com>
2025-01-17 12:09:54 +0100

    Small changes before merge

--- src/cleaninbox/api/api_backend.py
+++ src/cleaninbox/api/api_backend.py
@@ -10,20 +10,22 @@ from hydra import initialize, compose
 from typing import Optional, List
 from cleaninbox.data import text_dataset
 import anyio
+from torch.utils.data import DataLoader
 
 app = FastAPI()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global model, test_data, text_classes
+    global model, test_data, text_classes, cfg, DEVICE
 
     # Initialize Hydra configuration
     with initialize(config_path="../../configs", version_base="1.1"):
         cfg = compose(config_name="config")
     
     # Fetch model:
-    model = BertTypeClassification(cfg.model.name)
-    model.load_state_dict(torch.load(cfg.basic.current_model))
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = BertTypeClassification(cfg.model.name).to(DEVICE)
+    model.load_state_dict(torch.load(cfg.basic.model_ckpt, map_location=DEVICE))
     model.eval()
 
     # Fetch data:
@@ -35,10 +37,37 @@ async def lifespan(app: FastAPI):
         yield
 
     finally:
-        del model, test_data, text_classes
+        del model, test_data, text_classes, cfg, DEVICE
 
 app = FastAPI(lifespan=lifespan)
 
+def predict():
+    test_dataloader = DataLoader(test_data, batch_size = cfg.experiment.overfit.batch_size, shuffle=True)
+
+    correct = 0
+    total = 0
+
+    # Disable gradient calculations for evaluation
+    with torch.no_grad():
+        for input_ids, token_type_ids, attention_mask, labels in test_dataloader:
+            # Move inputs and labels to the computation device
+            input_ids = input_ids.to(DEVICE)
+            token_type_ids = token_type_ids.to(DEVICE)
+            attention_mask = attention_mask.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            # Forward pass to compute logits
+            logits = model(input_ids, attention_mask, token_type_ids)
+
+            # Predicted labels are the indices of the maximum logit
+            predictions = torch.argmax(logits, dim=1)
+
+            # Update the counters
+            correct += (predictions == labels).sum().item()
+            total += labels.size(0)
+
+    # Compute and print accuracy
+    accuracy = correct / total
 
 
 @app.get("/")
