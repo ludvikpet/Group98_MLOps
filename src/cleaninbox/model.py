9fc0791efa0c871ff6a481b5d7708afce7eacb1f
ludvikpet <ludvikpet@gmail.com>
2025-01-22 11:00:20 +0100

    Backend predict functionality added

--- src/cleaninbox/model.py
+++ src/cleaninbox/model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn 
+import torch
 from transformers import AutoModel, AutoTokenizer
 
 class BertTypeClassification(nn.Module):
@@ -26,4 +27,17 @@ class BertTypeClassification(nn.Module):
         #decodes a batch of input ids to a sentence in list format. Used for plotting topk distribution during training.
         return [self.tokenizer.decode(ids, skip_special_tokens=True) for ids in input_ids]
 
+def top_k_logits(logits, k):
+    '''
+    returns the top k logits (softmax) and their indices
 
+    Args:
+        logits: torch.tensor, shape (1, num_classes)
+        k: int, number of top logits to return
+    '''
+    softmax = nn.Softmax(dim=1)
+    softmax_logits = softmax(logits)
+    topk_values, topk_indices = torch.topk(softmax_logits, k, dim=1)
+    topk_values = topk_values.cpu().numpy().tolist()[0]
+    topk_indices = topk_indices.cpu().numpy().tolist()[0]
+    return topk_values, topk_indices
